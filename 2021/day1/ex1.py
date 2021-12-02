03a891823c26bc00babd9c56de2c517563f5d656
IrinaScop <i.scopouli@gmail.com>
2021-12-02 13:21:29 +0200

    initial commit

---
+++ 2021/day1/ex1.py
@@ -0,0 +1,23 @@
+# Python code to
+# demonstrate readlines()
+ 
+mylist = []
+ 
+# Using readlines()
+file = open('numbers_list.txt', 'r')
+Lines = file.readlines()
+ 
+# Strips the newline character
+for line in Lines:
+    mylist.append(int(line.strip()))
+    #print("Line{}: {}".format(count, line.strip()))
+
+count = 0
+
+for i in range(len(mylist)-1):
+    if mylist[i] < mylist[i+1]:
+        count = count + 1
+
+
+#print(mylist'\n')
+print(count)
---
+++ 2021/day1/ex2.py
@@ -0,0 +1,27 @@
+# Python code to
+# demonstrate readlines()
+ 
+mylist = []
+sum_list = []
+ 
+# Using readlines()
+file = open('numbers_list.txt', 'r')
+Lines = file.readlines()
+ 
+# Strips the newline character
+for line in Lines:
+    mylist.append(int(line.strip()))
+    #print("Line{}: {}".format(count, line.strip()))
+
+for i in range(0,len(mylist)-2,3):
+    sum_list.append(mylist[i] +mylist[i+1] + mylist[i+2])
+
+count = 0
+
+for i in range(len(sum_list)-1):
+    if sum_list[i] < sum_list[i+1]:
+        count = count + 1
+
+
+#print(mylist'\n')
+print(count)
