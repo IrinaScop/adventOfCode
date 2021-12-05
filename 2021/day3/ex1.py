7f38f5cfb1bd50dddb8adfd6f364dd9292c4ef90
IrinaScop <i.scopouli@gmail.com>
2021-12-05 13:07:21 +0200

    Update ex1.py

--- 2021/day3/ex1.py
+++ 2021/day3/ex1.py
@@ -1,39 +1,76 @@
-f=open("binary.txt","r")
+#-----------------------------------
+def createlist(txt, mode):
+#-----------------------------------
+    f=open(txt, mode)
 
-Lines=f.readlines()
-binary_list=[]
+    Lines=f.readlines()
+    binaryList=[]
 
-for line in Lines:
-    binary_list.append(line.rstrip())
-f.close()
+    for line in Lines:
+        binaryList.append(line.rstrip())
+    f.close()
 
-len_of_nummber = len(binary_list[1])
+    return binaryList
 
-gamma = ''
-epsilon = ''
 
-for i in range(len_of_nummber):
+#-------------------------------------------------- 
+def mostFrequentBit(mylist, i):
+#--------------------------------------------------
     zeros = 0
     ones = 0
-    for y in range(len(binary_list)):
 
-        if ((binary_list[y])[i]) == "0":
+    for y in range(len(mylist)):
+        if ((mylist[y])[i]) == "0":
             zeros = zeros + 1
-        elif ((binary_list[y])[i]) == "1":
+        elif ((mylist[y])[i]) == "1":
             ones = ones + 1
+    return zeros, ones
 
-    if zeros > ones:
-        gamma = gamma + "0"
-        epsilon= epsilon + "1"
-    elif ones > zeros:
-        gamma = gamma + "1"
-        epsilon= epsilon + "0"
+#-------------------------------------------------- 
+def findBinaryNumbers(mylist):
+#--------------------------------------------------
+    binaryList = mylist
 
-dec_gamma = int(gamma, 2)
-decimal_epsilon = int(epsilon, 2)
+    lenOflist = len(binaryList[1])
 
-print("Binary number gamma:", gamma, " is equal to decimal number:",dec_gamma)
-print("Binary number epsilon:", epsilon, " is equal to epsilon number:",decimal_epsilon)
+    print(lenOflist)
 
-print("The power consumption of the submarine is gamma multuplied by epsilon = ", (dec_gamma*decimal_epsilon))
+    gamma = ''
+    epsilon = ''
 
+    for i in range(lenOflist):
+
+        zeros, ones = mostFrequent(mylist, i)
+
+        if zeros > ones:
+            gamma = gamma + "0"
+            epsilon= epsilon + "1"
+            print(gamma)
+        elif ones > zeros:
+            gamma = gamma + "1"
+            epsilon= epsilon + "0"
+
+    return gamma, epsilon
+
+#-------------------------------------------------- 
+def main():
+#--------------------------------------------------
+    binaryList=[]
+
+    binaryList = createlist("binary.txt", "r")
+
+    gamma, epsilon = findBinaryNumbers(binaryList)
+
+    dec_gamma = int(gamma, 2)
+    decimal_epsilon = int(epsilon, 2)
+
+    print("Binary number gamma:", gamma, " is equal to decimal number:",dec_gamma)
+    print("Binary number epsilon:", epsilon, " is equal to epsilon number:",decimal_epsilon)
+
+    print("The power consumption of the submarine is gamma multuplied by epsilon = ", (dec_gamma*decimal_epsilon))
+
+
+if __name__ == "__main__":
+
+    # calling main function
+    main()
