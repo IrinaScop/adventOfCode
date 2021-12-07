5554315231d6b9e9b7b44f0a8c3d9505bb15e9cc
IrinaScop <i.scopouli@gmail.com>
2021-12-07 20:38:22 +0200

    Update ex2.py
    #todo

--- 2021/day3/ex2.py
+++ 2021/day3/ex2.py
@@ -4,61 +4,90 @@ def createlist(txt, mode):
     f=open(txt, mode)
 
     Lines=f.readlines()
-    binary_list=[]
+    binaryList=[]
 
     for line in Lines:
-        binary_list.append(line.rstrip())
+        binaryList.append(line.rstrip())
     f.close()
 
-    return binary_list
+    return binaryList
 
 #-------------------------------------------------- 
-def mostFrequent(mylist):
+def mostFrequentBit(mylist, index, commonValue):
 #--------------------------------------------------
-    binary_list = mylist
+    zeros = 0
+    ones = 0
+    mfb = ''
 
-    len_of_nummber = len(binary_list[1])
-
-    print(len_of_nummber)
-
-    gamma = ''
-    epsilon = ''
-
-    for i in range(len_of_nummber):
-        zeros = 0
-        ones = 0
-        for y in range(len(binary_list)):
-            if ((binary_list[y])[i]) == "0":
-                zeros = zeros + 1
-            elif ((binary_list[y])[i]) == "1":
-                ones = ones + 1
+    for row in range(len(mylist)-1):
+        print("ROUND: ",row)
+        if ((mylist[row])[index]) == "0":
+            zeros = zeros + 1
+        elif ((mylist[row])[index]) == "1":
+            ones = ones + 1
 
         if zeros > ones:
-            gamma = gamma + "0"
-            epsilon= epsilon + "1"
-            print(gamma)
-        elif ones > zeros:
-            gamma = gamma + "1"
-            epsilon= epsilon + "0"
+            mfb = "0"
+        elif zeros < ones:
+            mfb = "1"
+        
+        elif zeros == ones and commonValue == "least":
+                mfb = "0"
+        elif zeros == ones and commonValue == "most":
+                mfb = "1"
+    return mfb
 
-    return gamma, epsilon
+#-------------------------------------------------- 
+def createMultiplier(myList):
+#--------------------------------------------------
+    lenOflist = len(myList[1])
+    newList = []
+    pattern = ''
+
+    for i in range(lenOflist):
+        #find most commont bit and add to pattern
+        # append elements tha match pattern
+        #do it for all elements in (every)newList until there is one element left
+
+    return newList
+
+#-------------------------------------------------- 
+def multiplier(mylist):
+#--------------------------------------------------
+    oxygen = []
+    oc2 = mylist
+    lenOflist = len(mylist[1])
+
+    oxygen = createMultiplier(mylist)
+
+    #print(oxygen, oc2)
+
+    return oxygen
+
+#-------------------------------------------------- 
+def binaryToDecimal(binaryNumber):
+#--------------------------------------------------
+    print(int(str(binaryNumber[0])))
+    dec = (binaryNumber[0])
+    dec = int(str(binaryNumber[0]),2)
+    print(dec)
+
+    return int(dec)
 
 #-------------------------------------------------- 
 def main():
 #--------------------------------------------------
-    binary_list=[]
+    binaryList=[]
+    binaryList = createlist("binary_test.txt", "r")
+    oxygen = multiplier(binaryList)
 
-    binary_list = createlist("binary.txt", "r")
+    dec_oxygen = binaryToDecimal(oxygen)
+    #decimal_oc2 = binaryToDecimal(oc2)
+    print(dec_oxygen)
 
-    gamma, epsilon = mostFrequent(binary_list)
-
-    dec_gamma = int(gamma, 2)
-    decimal_epsilon = int(epsilon, 2)
-
-    print("Binary number gamma:", gamma, " is equal to decimal number:",dec_gamma)
-    print("Binary number epsilon:", epsilon, " is equal to epsilon number:",decimal_epsilon)
-
-    print("The power consumption of the submarine is gamma multuplied by epsilon = ", (dec_gamma*decimal_epsilon))
+    print("Binary number gamma:", oxygen, " is equal to decimal number:",dec_oxygen)
+    #print("Binary number epsilon:", oc2, " is equal to epsilon number:",decimal_oc2)
+    #print("The power consumption of the submarine is gamma multuplied by epsilon = ", (dec_gamma*decimal_epsilon))
 
 
 if __name__ == "__main__":
